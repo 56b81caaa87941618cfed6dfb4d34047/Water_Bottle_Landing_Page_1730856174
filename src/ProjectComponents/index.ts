751a1b5f49dafb236facbba3af12ea835fd4f11e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-11-05 19:23:00 -0600

    Add namespace components

---
+++ src/ProjectComponents/index.ts
@@ -0,0 +1,6 @@
+export * as Header1730856177 from './Header1730856177'
+export * as Hero1730856177 from './Hero1730856177'
+export * as Footer1730856177 from './Footer1730856177'
+export * as Footer1730856177 from './Footer1730856177'
+export * as Hero1730856177 from './Hero1730856177'
+export * as Header1730856177 from './Header1730856177'
